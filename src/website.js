e61bc855e07eacd8048b7ab1ca28407bcbf04f34
Jackie Yu <jackieyu247@gmail.com>
2022-10-14 15:01:08 +0100

    update style of header

--- src/website.js
+++ src/website.js
@@ -2,47 +2,67 @@ function createHeader(){
     // document elements
     const header = document.createElement('header');
     const headerStrip = document.createElement('div');
-    const nav = createNav();
     const title = document.createElement('h1');
-    // append to element
-    header.appendChild(headerStrip);
-    headerStrip.appendChild(nav);
-    headerStrip.appendChild(title);
-    // add classes to elements
+    const nav = createNav();
+    // CLASSES // 
     header.classList.add('header');
     headerStrip.classList.add('header-strip-container');
     // add text
     title.textContent = "Bonzai Ramen"
-    
+    // append to element
+    header.appendChild(headerStrip);
+    headerStrip.appendChild(title);
+    headerStrip.appendChild(nav);
+
     return header;
 }
-
 function createNav(){
+    // document elements
     const nav = document.createElement('nav');
+    const links = document.createElement('div');
     const homePage = document.createElement('a');
     const menu = document.createElement('a');
     const contact = document.createElement('a');
+    const dropdown = document.createElement('a');
+    const hamburger = document.createElement('i');
+    // CLASSES // 
+    nav.classList.add('topnav');
+    dropdown.classList.add('icon');
+    hamburger.classList.add('fa','fa-bars');
+    links.setAttribute('id','myLinks');
 
-    nav.appendChild(homePage);
-    nav.appendChild(menu);
-    nav.appendChild(contact);
+    dropdown.addEventListener('click', () => {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "flex") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "flex";
+        }
+    })
+    homePage.textContent = 'Home';
+    menu.textContent = 'Menu';
+    contact.textContent = 'Contact';
+
+    links.appendChild(homePage);
+    links.appendChild(menu);
+    links.appendChild(contact);
+    dropdown.appendChild(hamburger);
+    nav.appendChild(links);
+    nav.appendChild(dropdown);
 
     return nav;
 }
-
 function createMain(){
     const main = document.createElement('main');
+    // CLASSES // 
     main.classList.add('main');
+    main.setAttribute('id','main');
 
     return main;
 }
-
 export default function createWebsite(){
     const content = document.getElementById('content')
     const header = createHeader();
-    const nav = createNav();
-
-    header.appendChild(nav);
     content.appendChild(header);
     content.appendChild(createMain());
 }
