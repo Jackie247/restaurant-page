92a6485adcae74d81ff451ef6cfe6f880f79f8d4
Jackie Yu <jackieyu247@gmail.com>
2022-10-17 12:55:08 +0100

    update function

--- src/homepage.js
+++ src/homepage.js
@@ -1,7 +1,16 @@
 import {createImage,createParagraph} from './elements.js'
 import restaurantFace from './assets/restaurantFace.jpg';
+import gallery1 from './assets/gallery1.jpg';
+import gallery2 from './assets/gallery2.jpg';
+import gallery3 from './assets/gallery3.jpg';
+
 import stars from './assets/5stars.png';
 
+// restaurantFace credit Baiq Daling https://unsplash.com/photos/vuZJ1VRHNF4
+// gallery1 credit Anton Nazaretian https://unsplash.com/photos/JCHlsKSgbSk
+// gallery2 credit 
+// gallery3 credit Photo by <a href="https://unsplash.com/@s_tsuchiya?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">S. Tsuchiya</a> on <a href="https://unsplash.com/s/photos/japanese-restaurant-interior?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
 function createHero(){
     const content = document.createElement('div');
     const hero = document.createElement('div');
@@ -10,9 +19,8 @@ function createHero(){
     const heroTitle = document.createElement('h2');
     const heroTitleFoods = document.createElement('h4');
     const heroJapanese = document.createElement('h4');
-    const fiveStars = createImage(stars,'5 stars review');
 
-    content.classList.add('main-content')
+    content.classList.add('main-content-hero')
     hero.classList.add('hero');
     heroImage.classList.add('hero-image');
     heroTextDiv.classList.add('hero-text');
@@ -34,14 +42,38 @@ function createHero(){
 
 function createMid(){
     const content = document.createElement('div');
-    const firstImage = createImage();
+    const titleContainer = document.createElement('div');
+    const welcome = document.createElement('h2');
+    const welcomeJap = document.createElement('h4');
+    const gallery = document.createElement('div');
+    const firstImage = createImage(gallery1,'chefs cooking in the kitchen');
     const firstImageText = createParagraph('Taste authentic ramen created by chefs who honed their techniques across restaurants in Japan');
-    const secondImage = createImage();
-    const secondImageText = createParagraph();
-    const thirdImage = createImage();
-    const thirdImageText = createParagraph();
+    const secondImage = createImage(gallery2,'bowl of ramen');
+    const secondImageText = createParagraph('Taste authentic ramen created by chefs who honed their techniques across restaurants in Japan');
+    const thirdImage = createImage(gallery3);
+    const thirdImageText = createParagraph('Taste authentic ramen ');
 
-    content.appendChild(firstImage);
+    welcome.textContent = 'Welcome!';
+    welcomeJap.textContent = '私たちのサイトに、ようこそ！'
+
+    content.classList.add('main-content-mid')
+    titleContainer.classList.add('mid-section-title');
+    welcome.classList.add('welcome');
+    welcomeJap.classList.add('welcome-japanese');
+    gallery.classList.add('gallery');
+
+    titleContainer.appendChild(welcome);
+    titleContainer.appendChild(welcomeJap);
+    content.appendChild(titleContainer);
+
+    content.appendChild(gallery);
+    
+    gallery.appendChild(firstImage);
+    gallery.appendChild(firstImageText);
+    gallery.appendChild(secondImage);
+    gallery.appendChild(secondImageText);
+    gallery.appendChild(thirdImage);
+    gallery.appendChild(thirdImageText);
 
     return content;
 }
