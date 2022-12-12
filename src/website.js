af6b984e1357bb2553ecb31caeee4ffb2f57126c
Jackie Yu <jackieyu247@gmail.com>
2022-12-12 19:33:12 +0000

    reformat

--- src/website.js
+++ src/website.js
@@ -1,81 +1,81 @@
-import createHomePage from './homepage.js';
-import createMenuPage from './menu.js';
-import createContactPage from './contact.js';
-import {removeAllChildNodes} from './elements.js';
+import createHomePage from "./homepage.js";
+import createMenuPage from "./menu.js";
+import createContactPage from "./contact.js";
+import { removeAllChildNodes } from "./elements.js";
 
-function createHeader(){
-    // document elements
-    const header = document.createElement('header');
-    const nav = createNav();
-    // CLASSES // 
-    header.classList.add('header');
-    // append to element
-    header.appendChild(nav);
+function createHeader() {
+  // document elements
+  const header = document.createElement("header");
+  const nav = createNav();
+  // CLASSES //
+  header.classList.add("header");
+  // append to element
+  header.appendChild(nav);
 
-    return header;
+  return header;
 }
-function createNav(){
-    // document elements
-    const nav = document.createElement('nav');
-    const links = document.createElement('div');
-    const home = document.createElement('button');
-    const menu = document.createElement('button');
-    const contact = document.createElement('button');
-    // CLASSES // 
-    nav.classList.add('navbar');
-    links.classList.add('nav-links')
-    home.textContent = 'Home';
-    menu.textContent = 'Menu';
-    contact.textContent = 'Contact';
+function createNav() {
+  // document elements
+  const nav = document.createElement("nav");
+  const links = document.createElement("div");
+  const home = document.createElement("button");
+  const menu = document.createElement("button");
+  const contact = document.createElement("button");
+  // CLASSES //
+  nav.classList.add("navbar");
+  links.classList.add("nav-links");
+  home.textContent = "Home";
+  menu.textContent = "Menu";
+  contact.textContent = "Contact";
 
-    home.setAttribute('id','home-btn');
-    home.addEventListener('click',() => {
-        main.innerHTML = '';
-        createHomePage();
-    })
-    menu.setAttribute('id','menu-btn');
-    menu.addEventListener('click',() => {
-        main.innerHTML = '';
-        createMenuPage();
-    })
-    contact.setAttribute('id','contact-btn');
-    contact.addEventListener('click',() => {
-        main.textContent = '';
-        createContactPage();
-    })
-
-    links.appendChild(home);
-    links.appendChild(menu);
-    links.appendChild(contact);
-
-    nav.appendChild(links);
-
-    return nav;
-}
-function createMain(){
-    const main = document.createElement('main');
-    // CLASSES // 
-    main.classList.add('main');
-    main.setAttribute('id','main');
-
-    return main;
-}
-
-function createFooter(){
-    const footer = document.createElement('footer');
-    const text = document.createElement('h4');
-    text.textContent = 'Created by Jackie247';
-    footer.appendChild(text);
-
-    return footer;
-}
-
-export default function createWebsite(){
-    const content = document.getElementById('content')
-    const header = createHeader();
-    content.appendChild(header);
-    content.appendChild(createMain());
-    content.appendChild(createFooter());
-
+  home.setAttribute("id", "home-btn");
+  home.addEventListener("click", () => {
+    main.innerHTML = "";
     createHomePage();
-}   
+  });
+  menu.setAttribute("id", "menu-btn");
+  menu.addEventListener("click", () => {
+    main.innerHTML = "";
+    createMenuPage();
+  });
+  contact.setAttribute("id", "contact-btn");
+  contact.addEventListener("click", () => {
+    main.textContent = "";
+    createContactPage();
+  });
+
+  links.appendChild(home);
+  links.appendChild(menu);
+  links.appendChild(contact);
+
+  nav.appendChild(links);
+
+  return nav;
+}
+function createMain() {
+  const main = document.createElement("main");
+  // CLASSES //
+  main.classList.add("main");
+  main.setAttribute("id", "main");
+
+  return main;
+}
+
+function createFooter() {
+  const footer = document.createElement("footer");
+  const text = document.createElement("h4");
+  text.textContent = "Created by Jackie247";
+  footer.appendChild(text);
+
+  return footer;
+}
+
+export default function createWebsite() {
+  const content = document.getElementById("content");
+  const header = createHeader();
+  content.appendChild(header);
+  content.appendChild(createMain());
+  content.appendChild(createFooter());
+
+  createHomePage();
+}
